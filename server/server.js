b15e21eba9127200dc7dcc291908e266bc9c8791
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-10 14:55:04 -0400

    set up essential middleware for user authentication using passport

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('express-flash');
+const logger = require('morgan');
 
 require('dotenv').config({ path: './config/.env' });
+
+// passport config
+require('./config/passport')(passport);
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger('dev'));
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
