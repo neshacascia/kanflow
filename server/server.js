eed641fbfd3acf7ac40c8d7309966811b0c38dd8
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-10 15:55:48 -0400

    established connection to the database and configured sessions, ensuring that they are stored in the database

--- server/server.js
+++ server/server.js
@@ -7,16 +7,29 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('express-flash');
 const logger = require('morgan');
+const connectDB = require('./config/database');
 
 require('dotenv').config({ path: './config/.env' });
 
 // passport config
 require('./config/passport')(passport);
 
+connectDB();
+
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger('dev'));
+
+// sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongoUrl: process.env.DB_STRING }),
+  })
+);
 
 // passport middleware
 app.use(passport.initialize());
