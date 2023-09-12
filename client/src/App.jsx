35c4b9a002f617f29743d883a2ae172acb3b8051
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-12 10:20:14 -0400

    added /logout path to render the LogoutPage component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Root from './components/Root';
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
 import BoardPage from './pages/BoardPage';
+import LogoutPage from './pages/LogoutPage';
 import './App.css';
 
 const router = createBrowserRouter([
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
       { path: 'login', element: <AuthPage /> },
       { path: 'signup', element: <AuthPage /> },
       { path: 'board', element: <BoardPage /> },
+      { path: 'logout', element: <LogoutPage /> },
     ],
   },
 ]);
