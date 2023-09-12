6bf1dd66cb5e86f5563b0478e9480acfd759fa3f
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-11 20:04:34 -0400

    set up routes related to user authentication using the various methods from the authController

--- server/routes/home.js
+++ server/routes/home.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home');
+const authController = require('../controllers/auth');
 
 router.get('/', homeController.getIndex);
+
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
+
+router.get('/logout', authController.logout);
+
+router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postSignup);
 
 module.exports = router;
