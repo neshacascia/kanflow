3c474e245d201737a3224eb5fac05bea232d5a57
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-12 10:09:18 -0400

    used user state to ensure appropriate links only render based on if the user has been authenticated or not. links to login and signup only display for unauthenticated users. created link to /logout endpoint to display once user is logged in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,16 +3,22 @@ import { Link } from 'react-router-dom';
 import { Context } from '../context/Context';
 
 export default function Navbar() {
-  const { setAuthValue } = useContext(Context);
+  const { user, setAuthValue } = useContext(Context);
 
   return (
     <nav>
-      <Link to="/login" onClick={() => setAuthValue('Login')}>
-        Login
-      </Link>
-      <Link to="/signup" onClick={() => setAuthValue('Signup')}>
-        Signup
-      </Link>
+      {!user && (
+        <div>
+          <Link to="/login" onClick={() => setAuthValue('Login')}>
+            Login
+          </Link>
+          <Link to="/signup" onClick={() => setAuthValue('Signup')}>
+            Signup
+          </Link>{' '}
+        </div>
+      )}
+
+      {user && <Link to="/logout">Logout</Link>}
     </nav>
   );
 }
