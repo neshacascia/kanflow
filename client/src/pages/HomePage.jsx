8b3554d072bd7fdc5254d42e7454f0716af643b8
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-13 14:37:56 -0400

    implemented user authentication check in the HomePage component. makes a GET request to /user, if user is present, it updates isLoggedIn state to true and redirects authenticated users to the board page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { Context } from '../context/Context';
+
 export default function HomePage() {
+  const navigate = useNavigate();
+  const { setIsLoggedIn } = useContext(Context);
+
+  useEffect(() => {
+    async function checkAuthentication() {
+      try {
+        const res = await axios.get('/user', { withCredentials: true });
+        const { user } = res.data;
+
+        if (user) {
+          setIsLoggedIn(true);
+          navigate('/board');
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    checkAuthentication();
+  }, []);
+
   return (
     <>
       <h1>Unleash Your Productivity with kanflow</h1>
