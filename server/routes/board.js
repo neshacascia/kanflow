935315ee421293d4dcc3a1072f9110605ac319b9
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-13 14:24:24 -0400

    removed /user route in /board

--- server/routes/board.js
+++ server/routes/board.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const boardController = require('../controllers/board');
-const authController = require('../controllers/auth');
 const { ensureAuth } = require('../middleware/auth');
 
 router.get('/', ensureAuth, boardController.getIndex);
-
-router.get('/user', authController.getUser);
 
 module.exports = router;
