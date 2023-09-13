66a562e52e543851f8791c8162e1679c7b03a0d6
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-13 17:35:19 -0400

    removed function and replaced with conditional to check if user is present in localStorage to set loggedIn state to true and redirect users to the board page. updated UI to conditionally render nav with Login and Signup links if the user property is null

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,33 +1,33 @@
 import { useEffect, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import { Context } from '../context/Context';
 
 export default function HomePage() {
   const navigate = useNavigate();
-  const { setIsLoggedIn } = useContext(Context);
+  const { isLoggedIn, setIsLoggedIn, setAuthValue } = useContext(Context);
+  const user = localStorage.getItem('user');
 
-  useEffect(() => {
-    async function checkAuthentication() {
-      try {
-        const res = await axios.get('/user', { withCredentials: true });
-        const { user } = res.data;
-
-        if (user) {
-          setIsLoggedIn(true);
-          navigate('/board');
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    }
-
-    checkAuthentication();
-  }, []);
+  if (user) {
+    setIsLoggedIn(true);
+    navigate('/board');
+  }
 
   return (
     <>
-      <h1>Unleash Your Productivity with kanflow</h1>
+      {user === null && (
+        <main>
+          <nav>
+            <Link to="/login" onClick={() => setAuthValue('Login')}>
+              Login
+            </Link>
+            <Link to="/signup" onClick={() => setAuthValue('Signup')}>
+              Signup
+            </Link>{' '}
+          </nav>
+          <h1>Unleash Your Productivity with kanflow</h1>
+        </main>
+      )}
     </>
   );
 }
