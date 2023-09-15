2a67f0252f093e0fb6391ecc25ba9d590bce6cb3
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-15 11:12:26 -0400

    implemented function for setting boardDetails state value to 'new' if user is creating a new board

--- client/src/components/MenuModal.jsx
+++ client/src/components/MenuModal.jsx
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
-export default function MenuModal({ setDisplayMenuModal }) {
+export default function MenuModal({ setDisplayMenuModal, setBoardDetails }) {
+  function displayBoard(type) {
+    if (type === 'new') {
+      setBoardDetails('new');
+    }
+
+    setDisplayMenuModal(false);
+  }
+
   return (
     <div>
       <h3>All Boards</h3>
@@ -12,7 +20,7 @@ export default function MenuModal({ setDisplayMenuModal }) {
         onClick={() => setDisplayMenuModal(false)}
       />
 
-      <Link>+ Create New Board</Link>
+      <button onClick={() => displayBoard('new')}>+ Create New Board</button>
       <Link to="/logout">Logout</Link>
     </div>
   );
