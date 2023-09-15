a386c7e0d6d4c73a73a71a6be9a4859948ca42c3
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-15 11:15:34 -0400

    conditionally rendered modals onto BoardPage if their states are true

--- client/src/pages/BoardPage.jsx
+++ client/src/pages/BoardPage.jsx
@@ -2,10 +2,16 @@ import { useEffect, useContext } from 'react';
 import axios from 'axios';
 import { Context } from '../context/Context';
 import MenuModal from '../components/MenuModal';
+import BoardDetailsModal from '../components/BoardDetailsModal';
 
 export default function BoardPage() {
-  const { setIsLoggedIn, displayMenuModal, setDisplayMenuModal } =
-    useContext(Context);
+  const {
+    setIsLoggedIn,
+    displayMenuModal,
+    setDisplayMenuModal,
+    boardDetails,
+    setBoardDetails,
+  } = useContext(Context);
 
   useEffect(() => {
     async function checkAuthentication() {
@@ -29,7 +35,17 @@ export default function BoardPage() {
     <main>
       <h1>Platform Launch Board</h1>
       {displayMenuModal && (
-        <MenuModal setDisplayMenuModal={setDisplayMenuModal} />
+        <MenuModal
+          setDisplayMenuModal={setDisplayMenuModal}
+          setBoardDetails={setBoardDetails}
+        />
+      )}
+      {boardDetails && (
+        <BoardDetailsModal
+          boardDetails={boardDetails}
+          setBoardDetails={setBoardDetails}
+          setDisplayMenuModal={setDisplayMenuModal}
+        />
       )}
     </main>
   );
