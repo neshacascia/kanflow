f7ffe5aa7a5e15ad96df7a0737b0110472adf8da
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-16 01:47:09 -0400

    created createBoard enpoint, which creates a new board document in the database using the Board model. the data stored includes the board's name, columns and the userId of the authenticated user. redirects the user to back to the /board route

--- server/controllers/board.js
+++ server/controllers/board.js
@@ -1,7 +1,21 @@
 const path = require('path');
+const Board = require('../models/Board');
 
 module.exports = {
   getIndex: (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+  },
+  createBoard: async (req, res) => {
+    try {
+      await Board.create({
+        name: req.body.boardName,
+        columns: req.body.columnName,
+        userId: req.user.id,
+      });
+      console.log('Board has been added');
+      res.redirect('/board');
+    } catch (err) {
+      console.error(err);
+    }
   },
 };
--- server/routes/board.js
+++ server/routes/board.js
@@ -4,5 +4,6 @@ const boardController = require('../controllers/board');
 const { ensureAuth } = require('../middleware/auth');
 
 router.get('/', ensureAuth, boardController.getIndex);
+router.post('/createBoard', boardController.createBoard);
 
 module.exports = router;
