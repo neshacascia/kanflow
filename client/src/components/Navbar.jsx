9f40dffe71399fee92da38413abf86cb51606315
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-18 11:33:33 -0400

    added plus icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { Context } from '../context/Context';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsisVertical, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
   const { isLoggedIn, setDisplayMenuModal, setBoardDetails } =
@@ -16,7 +16,10 @@ export default function Navbar() {
   return (
     <nav>
       {isLoggedIn && (
-        <FontAwesomeIcon icon={faEllipsisVertical} onClick={displayMenu} />
+        <span>
+          <FontAwesomeIcon icon={faPlus} />
+          <FontAwesomeIcon icon={faEllipsisVertical} onClick={displayMenu} />
+        </span>
       )}
     </nav>
   );
