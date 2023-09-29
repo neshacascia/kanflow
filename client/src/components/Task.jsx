8e7e8683680f5a7cf88d98052c54740837bf98a3
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-29 10:26:24 -0400

    rendered the total number of subtasks that are completed

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,8 +1,12 @@
 export default function Task({ task }) {
+  const completedSubtasks = task.subtasks.filter(
+    task => task.completed === true
+  ).length;
+
   return (
     <div>
       <p>{task.title}</p>
-      <span>{`of ${task.subtasks.length} subtasks`}</span>
+      <span>{`${completedSubtasks} of ${task.subtasks.length} subtasks`}</span>
     </div>
   );
 }
