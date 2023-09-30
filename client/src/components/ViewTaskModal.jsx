6406c4dea59a88329b1f0dd650b2356fcc17545c
Nesha Mervin <nesha.mervin@gmail.com>
2023-09-30 11:42:12 -0400

    implement updateCurrentStatus function to execute whenever the task status has changed. it will make a PUT request to /board/updateStatus endpoint sending the task's id and the new status

--- client/src/components/ViewTaskModal.jsx
+++ client/src/components/ViewTaskModal.jsx
@@ -1,7 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 export default function ViewTaskModal({ task, columns, selectedStatus }) {
+  const navigate = useNavigate();
   const completedSubtasks = task.subtasks.filter(
     task => task.completed === true
   ).length;
+
+  async function updateCurrentStatus(newStatus) {
+    try {
+      const res = await axios.put('/board/updateStatus', {
+        taskId: task._id,
+        status: newStatus,
+      });
+      console.log(res);
+      navigate(0);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <div>
@@ -22,7 +39,10 @@ export default function ViewTaskModal({ task, columns, selectedStatus }) {
 
       <label>
         Current Status
-        <select name="status">
+        <select
+          name="status"
+          onChange={e => updateCurrentStatus(e.target.value)}
+        >
           <option value={selectedStatus}>{selectedStatus}</option>
           {columns.map(status => {
             if (status !== selectedStatus) {
