47eca471ec45796e4fc58228d4a97cfff63271af
Nesha Mervin <nesha.mervin@gmail.com>
2023-10-02 10:51:23 -0400

    created subtasks state to ensure the UI reflects the correct status immediately after the checkbox is clicked. within the setCompletionStatus function, updated the local state prior to sending the update request to the server

--- client/src/components/ViewTaskModal.jsx
+++ client/src/components/ViewTaskModal.jsx
@@ -1,7 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useState } from 'react';
 
 export default function ViewTaskModal({ task, columns, selectedStatus }) {
+  const [subtasks, setSubtasks] = useState(task.subtasks);
+
   const navigate = useNavigate();
   const completedSubtasks = task.subtasks.filter(
     task => task.completed === true
@@ -9,6 +12,15 @@ export default function ViewTaskModal({ task, columns, selectedStatus }) {
 
   async function setCompletionStatus(id, completed) {
     try {
+      const updatedSubtasks = subtasks.map(subtask => {
+        if (subtask.id === id) {
+          return { ...subtask, completed: !completed };
+        }
+        return subtask;
+      });
+
+      setSubtasks(updatedSubtasks);
+
       const res = await axios.put('/board/setCompletionStatus', {
         taskId: task._id,
         subtaskId: id,
@@ -43,7 +55,7 @@ export default function ViewTaskModal({ task, columns, selectedStatus }) {
       </span>
 
       <ul>
-        {task.subtasks.map(subtask => (
+        {subtasks.map(subtask => (
           <li>
             <input
               type="checkbox"
@@ -51,6 +63,7 @@ export default function ViewTaskModal({ task, columns, selectedStatus }) {
               onChange={() =>
                 setCompletionStatus(subtask.id, subtask.completed)
               }
+              checked={subtask.completed}
             />{' '}
             {subtask.subtask}
           </li>
