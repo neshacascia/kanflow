684679e9f806fd2834df150b004d4654fb20d855
Nesha Mervin <nesha.mervin@gmail.com>
2023-11-10 10:36:49 -0500

    removed previous rendering of component

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -112,7 +112,6 @@ export default function Board() {
           selectedStatus={selectedStatus}
         />
       )}
-      {displayMenuModal === 'editBoard' && <BoardDetailsModal board={board} />}
     </main>
   );
 }
