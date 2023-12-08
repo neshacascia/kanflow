708de69cfb6323b5f02a73656554f13d17cc641d
Nesha Mervin <nesha.mervin@gmail.com>
2023-12-08 10:54:45 -0500

    rendered circle icon beside each column name and added gap between each task

--- client/src/components/Column.jsx
+++ client/src/components/Column.jsx
@@ -1,5 +1,8 @@
 import Task from './Task';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+
 export default function Column({
   name,
   tasks,
@@ -10,15 +13,21 @@ export default function Column({
 
   return (
     <section>
-      <h3 className="text-mediumGrey text-xs font-medium uppercase tracking-widest mb-6">{`${name} (${columnTasks.length})`}</h3>
-      {columnTasks.map((task, ind) => (
-        <Task
-          key={ind}
-          task={task}
-          setViewTask={setViewTask}
-          setSelectedStatus={setSelectedStatus}
-        />
-      ))}
+      <div className="flex gap-3">
+        <FontAwesomeIcon icon={faCircle} className="w-[15px] h-[15px]" />
+        <h3 className="text-mediumGrey text-xs font-medium uppercase tracking-widest mb-6">{`${name} (${columnTasks.length})`}</h3>
+      </div>
+
+      <div className="flex flex-col gap-5">
+        {columnTasks.map((task, ind) => (
+          <Task
+            key={ind}
+            task={task}
+            setViewTask={setViewTask}
+            setSelectedStatus={setSelectedStatus}
+          />
+        ))}
+      </div>
     </section>
   );
 }
