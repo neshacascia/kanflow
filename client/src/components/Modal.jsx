b5fa7920911e5d75ac11fa6544a7753dbe5d10fa
Nesha Mervin <nesha.mervin@gmail.com>
2023-12-13 21:30:08 -0500

    created a function handleModal click which calls e.stopPropagation() to prevent the clicks inside the modal content from triggering the closeModal function. only clicks on the black background will close the modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -4,12 +4,16 @@ import { Context } from '../context/Context';
 export default function Modal({ children }) {
   const { closeModal } = useContext(Context);
 
+  function handleModalClick(e) {
+    e.stopPropagation();
+  }
+
   return (
     <div
       onClick={closeModal}
       className="bg-black bg-opacity-50 w-screen h-screen fixed top-0 left-0 flex flex-col justify-center items-center z-50"
     >
-      {children}
+      <div onClick={handleModalClick}>{children}</div>
     </div>
   );
 }
