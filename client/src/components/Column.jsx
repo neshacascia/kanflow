a23a23e88002980262b3ffcc37b124d238f64b8b
Nesha Mervin <nesha.mervin@gmail.com>
2023-12-18 11:40:59 -0500

    added optional chaining operator to only assign the result of the operations if tasks is available instead of throwing an error

--- client/src/components/Column.jsx
+++ client/src/components/Column.jsx
@@ -10,17 +10,17 @@ export default function Column({
   setSelectedStatus,
   openModal,
 }) {
-  const columnTasks = tasks.filter(task => task.status === name);
+  const columnTasks = tasks?.filter(task => task.status === name);
 
   return (
     <section>
       <div className="flex gap-3">
         <FontAwesomeIcon icon={faCircle} className="w-[15px] h-[15px]" />
-        <h3 className="text-mediumGrey text-xs font-medium uppercase tracking-widest mb-6">{`${name} (${columnTasks.length})`}</h3>
+        <h3 className="text-mediumGrey text-xs font-medium uppercase tracking-widest mb-6">{`${name} (${columnTasks?.length})`}</h3>
       </div>
 
       <div className="flex flex-col gap-5">
-        {columnTasks.map((task, ind) => (
+        {columnTasks?.map((task, ind) => (
           <Task
             key={ind}
             task={task}
