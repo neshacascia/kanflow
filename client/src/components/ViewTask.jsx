9dfa01537c960c7cdde3f466c66dbaa7c5751bdf
Nesha Mervin <nesha.mervin@gmail.com>
2023-12-20 07:07:55 -0500

    added pointer cursor when hovering over status select element

--- client/src/components/ViewTask.jsx
+++ client/src/components/ViewTask.jsx
@@ -127,7 +127,7 @@ export default function ViewTask({
           <select
             name="status"
             onChange={e => updateCurrentStatus(e.target.value)}
-            className="bg-transparent text-white text-[13px] font-light border-[1px] rounded border-borderGrey py-3 px-4 mt-2"
+            className="bg-transparent text-white text-[13px] font-light border-[1px] rounded border-borderGrey py-3 px-4 mt-2 hover:cursor-pointer "
           >
             <option value={selectedStatus}>{selectedStatus}</option>
             {columns.map((status, ind) => {
