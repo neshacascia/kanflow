ea08bb58561ae15f8d8e12fe13ef45a2a6a9d578
Nesha Mervin <nesha.mervin@gmail.com>
2023-12-21 11:32:44 -0500

    decreased font size

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ export default function Navbar() {
     >
       <span className="h-full flex items-center">
         <img src={logo} />
-        <p className="text-white text-2xl font-semibold tracking-wide px-4 hidden md:block">
+        <p className="text-white text-xl font-semibold tracking-wide px-4 hidden md:block">
           kanflow
         </p>
         <div className="h-full border-r-[1px] border-linesDark mr-4 hidden md:block"></div>
