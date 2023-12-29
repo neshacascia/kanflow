37238f9bac1d87b8d34d7b437c9d1e932e9f4e0d
Nesha Mervin <nesha.mervin@gmail.com>
2023-12-29 13:31:20 -0500

    styled h2

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -20,12 +20,14 @@ export default function AuthPage() {
   return (
     <section className="h-screen relative flex items-center justify-evenly pt-16 md:pt-20">
       <img src={kanflowImg} className="h-80" />
-      <div>
-        <h2>{authValue}</h2>
+      <div className="flex flex-col gap-3">
+        <h2 className="text-2xl font-semibold">
+          {authValue === 'Login' ? 'Welcome back!' : 'Sign Up!'}
+        </h2>
         <p>
           {authValue === 'Signup'
             ? 'Enter your details to create an account'
-            : 'Enter your details to log in to your account'}
+            : 'Please login to your account'}
         </p>
 
         {/* <% if (locals.messages.errors) { %> <% messages.errors.forEach( el => {
