bb0dcd25bf9595db236e1130080aca2a4fa0dd17
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-10 19:01:00 -0500

    implemented theme toggling functionality with useEffect and localStorage. theme is set to dark by default and is applied to the document's root element class. the toggleTheme function toggles the theme by updating isDarkMode state

--- client/src/components/ToggleThemeButton.jsx
+++ client/src/components/ToggleThemeButton.jsx
@@ -1,14 +1,19 @@
+import { useContext } from 'react';
+import { Context } from '../context/Context';
+
 import sunIcon from '../assets/sun.svg';
 import moonIcon from '../assets/moon.svg';
 
 export default function ToggleThemeButton() {
+  const { toggleTheme } = useContext(Context);
+
   return (
     <div className="bg-veryDarkGrey flex justify-center gap-6 rounded-md py-3 mx-4">
       <img src={sunIcon} />
 
       <label className="w-12 h-6 relative inline-block">
         <input type="checkbox" className=" w-0 h-0 opacity-0" />
-        <span className="slider round"></span>
+        <span onClick={toggleTheme} className="slider round"></span>
       </label>
 
       <img src={moonIcon} />
