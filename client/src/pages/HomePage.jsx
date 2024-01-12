49909c3830ffbb52fc180fce5115191989c7c8c0
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-12 13:22:20 -0500

    moved call to action buttons below h1, added gradient to productivity text and updated font family

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,6 +3,8 @@ import { useNavigate, Link } from 'react-router-dom';
 import { Context } from '../context/Context';
 import axios from 'axios';
 
+import kanflow from '../assets/app-screenshot.png';
+
 export default function HomePage() {
   const navigate = useNavigate();
   const { setIsLoggedIn, storeAuthValue } = useContext(Context);
@@ -28,15 +30,18 @@ export default function HomePage() {
   return (
     <>
       {user === null && (
-        <main className="h-screen relative pt-16 md:pt-20">
-          <Link to="/login" onClick={() => storeAuthValue('login')}>
-            Login
-          </Link>
-          <Link to="/signup" onClick={() => storeAuthValue('signup')}>
-            Signup
-          </Link>{' '}
-          <button onClick={demoUserLogin}>Demo</button>
-          <h1>Unleash Your Productivity with kanflow</h1>
+        <main className="text-white bg-veryDarkGrey h-screen relative flex flex-col items-center pt-16 md:pt-20">
+          <h1 className="font-home text-6xl font-semibold tracking-wider leading-[65px] text-center w-[800px] pt-20">
+            Unleash your <span className="gradient">productivity</span> with
+            kanflow
+          </h1>
+
+          <div className="flex items-center gap-6 pt-10">
+            <Link to="/signup" onClick={() => storeAuthValue('signup')}>
+              Get Started
+            </Link>{' '}
+            <button onClick={demoUserLogin}>Try Demo</button>
+          </div>
         </main>
       )}
     </>
