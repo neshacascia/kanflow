47f75a7c6fb610b7588fd351fe5e9885cbffe20c
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-12 19:36:28 -0500

    updated features description styling

--- client/src/components/Feature.jsx
+++ client/src/components/Feature.jsx
@@ -1,8 +1,8 @@
 export default function Feature({ heading, description }) {
   return (
     <div className="w-[450px]">
-      <h3>{heading}</h3>
-      <p>{description}</p>
+      <h3 className="text-2xl font-home font-semibold">{heading}</h3>
+      <p className="text-gray-700 text-[15px] font-light pt-3">{description}</p>
     </div>
   );
 }
