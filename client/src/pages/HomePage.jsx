1413a6443f53bc878e1b602d7382a06313e11c98
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-12 19:25:03 -0500

    added features section and text

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -30,35 +30,51 @@ export default function HomePage() {
   return (
     <>
       {user === null && (
-        <main className="text-white bg-veryDarkGrey h-full relative flex flex-col items-center pt-16 md:pt-20">
-          <h1 className="font-home text-6xl font-semibold tracking-wider leading-[65px] text-center w-[800px] pt-14">
-            Unleash your <span className="gradient">productivity</span> with
-            kanflow
-          </h1>
-          <p className="text-gray-400 text-[18px] pt-4">
-            The ultimate kanban task management solution for seamless and
-            efficient workflow.
-          </p>
+        <main>
+          <section className="text-white bg-veryDarkGrey h-full relative flex flex-col items-center pt-16 md:pt-20">
+            <h1 className="font-home text-6xl font-semibold tracking-wider leading-[65px] text-center w-[800px] pt-14">
+              Unleash your <span className="gradient">productivity</span> with
+              kanflow
+            </h1>
+            <p className="text-gray-400 text-[18px] pt-4">
+              The ultimate kanban task management solution for seamless and
+              efficient workflow.
+            </p>
 
-          <div className="flex items-center gap-6 pt-10">
-            <Link
-              to="/signup"
-              onClick={() => storeAuthValue('signup')}
-              className="bg-mainPurple tracking-wider rounded py-3 px-8 hover:bg-mainPurpleHover"
-            >
-              Get Started
-            </Link>{' '}
-            <button
-              onClick={demoUserLogin}
-              className="tracking-wider border border-[#4568dc] rounded py-3 px-8"
-            >
-              Explore Demo
-            </button>
-          </div>
+            <div className="flex items-center gap-6 pt-10">
+              <Link
+                to="/signup"
+                onClick={() => storeAuthValue('signup')}
+                className="bg-mainPurple tracking-wider rounded py-3 px-8 hover:bg-mainPurpleHover"
+              >
+                Get Started
+              </Link>{' '}
+              <button
+                onClick={demoUserLogin}
+                className="tracking-wider border border-[#4568dc] rounded py-3 px-8"
+              >
+                Explore Demo
+              </button>
+            </div>
 
-          <div className="w-[1040px] py-20">
-            <img src={kanflow} className="w-full rounded-lg" />
-          </div>
+            <div className="w-[1040px] py-20">
+              <img src={kanflow} className="w-full rounded-lg" />
+            </div>
+          </section>
+
+          <section className="bg-lightGrey flex flex-col px-20">
+            <h2 className="text-[#273762] font-home text-5xl font-semibold tracking-wider pt-20">
+              Features&#8212;
+            </h2>
+            <p className="text-gray-800 text-[17px] tracking-wide w-[700px] pt-4">
+              Kanflow embraces simiplicity and minimalism, providing users with
+              a clean and easy-to-navigate workspace. By focusing on the
+              essentials, we empower users with a solution that enhances
+              productivity and promotes an smooth workflow
+            </p>
+
+            <div className="grid grid-cols-2 justify-center pt-10"></div>
+          </section>
         </main>
       )}
     </>
