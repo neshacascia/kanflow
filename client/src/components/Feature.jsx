4ea7796b4a2ee89c041788a7a78e66df2a997238
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-15 07:26:33 -0500

    more adjustments for tablet devices

--- client/src/components/Feature.jsx
+++ client/src/components/Feature.jsx
@@ -1,15 +1,15 @@
 export default function Feature({ icon, heading, description }) {
   return (
-    <div className="w-[400px] md:w-[450px] flex items-start gap-4">
+    <div className="w-[400px] md:w-[355px] lg:w-[450px] flex items-start gap-4 md:gap-3">
       <div className="bg-gray-200 rounded-full">
         <img
           src={icon}
-          className="max-w-[50px] md:max-w-[60px] p-4 object-contain"
+          className="max-w-[50px] lg:max-w-[60px] p-4 object-contain"
         />
       </div>
 
       <div>
-        <h3 className="text-[#372c51] text-xl md:text-2xl font-home font-semibold">
+        <h3 className="text-[#372c51] text-xl md:text-[22px] lg:text-2xl font-home font-semibold">
           {heading}
         </h3>
         <p className="text-gray-700 text-sm md:text-[15px] font-light pt-3">
