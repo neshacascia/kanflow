08c11014c9bea0fe85888f54f522d6eec031c63c
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-15 11:54:36 -0500

    decreased width of container for the form

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -101,7 +101,7 @@ export default function AuthPage() {
   return (
     <section className="h-screen relative flex flex-col items-center justify-center pt-44 mb-28 md:flex-row md:justify-between lg:justify-between md:pt-20 md:-mb-28">
       <img src={kanflowImg} className="h-44 md:h-60 md:pl-4 lg:h-80 xl:pl-20" />
-      <div className="md:bg-[#E6E6E6] md:w-[50%] md:h-full flex flex-col items-center md:items-stretch justify-center gap-3 rounded-lg py-5 md:py-8 md:px-10 lg:px-32">
+      <div className="md:bg-[#E6E6E6] md:w-[45%] md:h-full flex flex-col items-center md:items-stretch justify-center gap-3 rounded-lg py-5 md:py-8 md:px-10 lg:px-32">
         <h2 className="text-xl font-home md:text-3xl font-bold tracking-wide">
           {authValue === 'login' ? 'Welcome back!' : 'Sign Up!'}
         </h2>
