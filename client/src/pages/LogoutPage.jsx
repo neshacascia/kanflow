f03f595bb81a297501a2c0a241bf821ef4e16d99
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-15 12:53:21 -0500

    removed code for spinner and moved it to it's own component LoadingSpinner

--- client/src/pages/LogoutPage.jsx
+++ client/src/pages/LogoutPage.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { Context } from '../context/Context';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 export default function LogoutPage() {
   const { setIsLoggedIn, setIsDarkMode } = useContext(Context);
@@ -28,20 +29,7 @@ export default function LogoutPage() {
   return (
     <section className="text-white bg-veryDarkGrey font-semibold text-lg w-screen h-screen relative flex flex-col justify-center items-center pt-16 md:pt-20">
       <p>Logging out...</p>
-      <div class="sk-circle">
-        <div class="sk-circle1 sk-child"></div>
-        <div class="sk-circle2 sk-child"></div>
-        <div class="sk-circle3 sk-child"></div>
-        <div class="sk-circle4 sk-child"></div>
-        <div class="sk-circle5 sk-child"></div>
-        <div class="sk-circle6 sk-child"></div>
-        <div class="sk-circle7 sk-child"></div>
-        <div class="sk-circle8 sk-child"></div>
-        <div class="sk-circle9 sk-child"></div>
-        <div class="sk-circle10 sk-child"></div>
-        <div class="sk-circle11 sk-child"></div>
-        <div class="sk-circle12 sk-child"></div>
-      </div>
+      <LoadingSpinner />
     </section>
   );
 }
