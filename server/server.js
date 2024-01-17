9f34060e04ed2145ef2e34fcf27595d952244aa7
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-17 11:57:06 -0500

    imported the default export and used MongoStore.create instead of creating a new instance of MongoStore

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default;
 const flash = require('express-flash');
 const logger = require('morgan');
 const connectDB = require('./config/database');
@@ -27,7 +27,7 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongoUrl: process.env.DB_STRING }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
   })
 );
 
