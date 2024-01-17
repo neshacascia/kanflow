9441ca8af9e8e334a93cdf9c64b6fb617273d4d2
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-17 13:48:05 -0500

    corrected protocol to http

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ require('./config/passport')(passport);
 
 app.use(
   cors({
-    origin: 'https://localhost:5173',
+    origin: 'http://localhost:5173',
     credentials: true,
     exposedHeaders: ['Access-Control-Allow-Origin'],
   })
