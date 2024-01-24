b1f9156852b75b7a5e4689d18602e9f77fc31b76
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-24 14:46:04 -0500

    reset

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const flash = require('express-flash');
 const logger = require('morgan');
 const connectDB = require('./config/database');
 const homeRoutes = require('./routes/home');
@@ -17,12 +15,8 @@ require('dotenv').config({ path: './config/.env' });
 // passport config
 require('./config/passport')(passport);
 
-const corsOptions = {
-  origin: 'https://nc-kanflow.onrender.com',
-  credentials: true,
-};
+connectDB();
 
-app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -42,13 +36,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(flash());
-
 app.use('/api', homeRoutes);
 app.use('/api/board', boardRoutes);
 
-connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`The server is running on port ${process.env.PORT}.`);
-  });
+app.listen(process.env.PORT, () => {
+  console.log(`The server is running on port ${process.env.PORT}.`);
 });
