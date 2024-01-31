5e2f097d59c32dd9e1955016047e7fe85149a87e
Nesha Mervin <nesha.mervin@gmail.com>
2024-01-31 16:55:18 -0500

    added else block to navigate user back to homepage if not authenticated

--- client/src/pages/BoardPage.jsx
+++ client/src/pages/BoardPage.jsx
@@ -38,6 +38,9 @@ export default function BoardPage() {
           } else {
             setWelcomeMessage(true);
           }
+        } else {
+          navigate('/');
+          console.log('User is not logged in');
         }
       } catch (err) {
         console.error(err);
