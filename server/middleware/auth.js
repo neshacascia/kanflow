950c0a9e604e73914ad963c6998de7704748af63
Nesha Mervin <nesha.mervin@gmail.com>
2024-02-01 07:11:56 -0500

    ensureAuth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     if (req.isAuthenticated()) {
       return next();
     } else {
-      res.redirect('/');
+      res.redirect('https://kanflow.onrender.com/');
     }
   },
   ensureGuest: function (req, res, next) {
