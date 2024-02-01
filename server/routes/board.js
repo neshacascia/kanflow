2841206c927521b16981d740e119f7fea5068f85
Nesha Mervin <nesha.mervin@gmail.com>
2024-02-01 06:57:19 -0500

    redirect to / from /board if unauthenticated

--- server/routes/board.js
+++ server/routes/board.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const boardController = require('../controllers/board');
-const { ensureAuth } = require('../middleware/auth');
+const { ensureGuest, ensureAuth } = require('../middleware/auth');
+
+router.get('/', ensureGuest, (req, res) => {
+  res.redirect('/');
+});
 
 router.get('/', ensureAuth, boardController.getIndex);
 router.get('/getBoards', boardController.getBoards);
