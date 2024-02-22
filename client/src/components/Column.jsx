af888243e6f8f755a5cd1f19b92dae259d0eddcc
Nesha Mervin <nesha.mervin@gmail.com>
2024-02-22 10:00:50 -0500

    wrapped Task component inside SortableContext and added null check before accessing status property

--- client/src/components/Column.jsx
+++ client/src/components/Column.jsx
@@ -1,10 +1,12 @@
 import Task from './Task';
-import { SortableContext } from '@dnd-kit/sortable';
+import { SortableContext, useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function Column({
+  id,
   ind,
   name,
   tasks,
@@ -12,7 +14,7 @@ export default function Column({
   setSelectedStatus,
   openModal,
 }) {
-  const columnTasks = tasks?.filter(task => task.status === name);
+  const columnTasks = tasks?.filter(task => task?.status === name);
 
   const columnColours = [
     'text-red-500',
@@ -25,19 +27,19 @@ export default function Column({
   ];
 
   return (
-    <SortableContext items={columnTasks.map(task => task._id)}>
-      <section className="min-w-[280px] pr-4">
-        <div className="flex gap-3">
-          <FontAwesomeIcon
-            icon={faCircle}
-            className={`w-[15px] h-[15px] ${columnColours[ind]}`}
-          />
-          <h3 className="text-mediumGrey text-xs font-medium uppercase tracking-widest mb-6">{`${name} (${
-            columnTasks ? columnTasks.length : ''
-          })`}</h3>
-        </div>
+    <section className="min-w-[280px] pr-4">
+      <div className="flex gap-3">
+        <FontAwesomeIcon
+          icon={faCircle}
+          className={`w-[15px] h-[15px] ${columnColours[ind]}`}
+        />
+        <h3 className="text-mediumGrey text-xs font-medium uppercase tracking-widest mb-6">{`${name} (${
+          columnTasks && columnTasks.length
+        })`}</h3>
+      </div>
 
-        <div className="flex flex-col gap-5 last:pb-8">
+      <div className="flex flex-col gap-5 last:pb-8">
+        <SortableContext items={columnTasks?.map(task => task._id)}>
           {columnTasks?.map((task, ind) => (
             <Task
               key={ind}
@@ -47,8 +49,8 @@ export default function Column({
               openModal={openModal}
             />
           ))}
-        </div>
-      </section>
-    </SortableContext>
+        </SortableContext>
+      </div>
+    </section>
   );
 }
