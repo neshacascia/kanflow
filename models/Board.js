f74e3b00e997e9e2db80610c40c2b4b74c6d2464
Nesha Mervin <nesha.mervin@gmail.com>
2024-03-07 07:29:27 -0500

    updated Board schema to have only userId and boards property in database

--- models/Board.js
+++ models/Board.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose');
 
 const BoardSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  columns: {
-    type: Array,
-    required: true,
-  },
   userId: {
     type: String,
     required: true,
+  },
+  boards: {
+    type: Array,
   },
 });
 
