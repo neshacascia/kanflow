fc36be31a7cc68871932edbf0ba5bb29cd4dc69a
Nesha Mervin <nesha.mervin@gmail.com>
2024-06-03 13:43:50 -0400

    implemented password update logic. validates the current password, hashes the new password and updates the user's password in the databased if the current password is valid

--- controllers/account.js
+++ controllers/account.js
@@ -1,9 +1,10 @@
+const bcrypt = require('bcrypt');
 const path = require('path');
 const User = require('../models/User');
-const { ObjectId } = require('mongodb');
 
 module.exports = {
   updateAccount: async (req, res) => {
+    const user = await User.findById(req.user.id);
     const email = req.body.email != req.user.email ? req.body.email : null;
 
     try {
@@ -17,9 +18,44 @@ module.exports = {
             new: true,
           }
         );
+        console.log("User's email has been updated");
       }
-      console.log("User's email has been updated");
-      res.status(200).json("User's email has been updated");
+      if (req.body.currentPassword) {
+        const isMatch = await bcrypt.compare(
+          req.body.currentPassword,
+          user.password
+        );
+
+        if (!isMatch) {
+          console.log('Invalid current password');
+          return res.status(401).json({ msg: 'Invalid current password' });
+        } else {
+          if (req.body.newPassword) {
+            bcrypt.genSalt(10, async (err, salt) => {
+              try {
+                const hash = await bcrypt.hash(req.body.newPassword, salt);
+                newPassword = hash;
+
+                await User.findOneAndUpdate(
+                  {
+                    _id: req.user.id,
+                  },
+                  {
+                    password: newPassword,
+                  },
+                  {
+                    new: true,
+                  }
+                );
+                console.log("User's password has been updated");
+              } catch (err) {
+                console.error(err);
+              }
+            });
+          }
+        }
+      }
+      res.status(200).json({ msg: "User's account has been updated" });
     } catch (err) {
       console.error(err);
     }
