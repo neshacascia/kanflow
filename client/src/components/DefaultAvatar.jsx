ba53feaf9e06fb0c56a5f3ec1aef7f4b8ddc9287
Nesha Mervin <nesha.mervin@gmail.com>
2024-06-25 09:26:38 -0400

    created DefaultAvatar component to display a default user avatar using FontAwesome icon. accepts a size prop to set the width and height of the avatar container

---
+++ client/src/components/DefaultAvatar.jsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+export default function DefaultAvatar({ size }) {
+  return (
+    <div
+      className={`bg-[#706dc2da] w-${size} h-${size} flex justify-center items-center rounded-full mr-2`}
+    >
+      <FontAwesomeIcon icon={faUser} className="text-white text-lg" />
+    </div>
+  );
+}
