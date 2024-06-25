9afc943e7d099e133cf5fa2dfdff2cf570f64493
Nesha Mervin <nesha.mervin@gmail.com>
2024-06-25 10:17:18 -0400

    decreased height or nav border line

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -139,7 +139,7 @@ export default function Navbar() {
               />
             </button>
 
-            <div className="h-20 border-linesLight dark:border-linesDark border-l mx-4"></div>
+            <div className="h-16 md:h-20 border-linesLight dark:border-linesDark border-l mx-4"></div>
 
             <button
               onClick={() =>
