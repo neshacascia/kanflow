e685a3355b656e8bae4490fc0fb37a5a5c22266b
Nesha Mervin <nesha.mervin@gmail.com>
2024-06-25 09:17:11 -0400

    added route for updating user avatar, handling the file upload for the image

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../middleware/multer');
 const accountController = require('../controllers/account');
 const { ensureAuth } = require('../middleware/auth');
 
 router.put('/updateAccount', ensureAuth, accountController.updateAccount);
+router.post(
+  '/updateAvatar',
+  upload.single('avatar'),
+  accountController.updateAvatar
+);
 
 module.exports = router;
