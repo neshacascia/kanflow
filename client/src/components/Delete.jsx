303d6a077f59bc7d461de03aaa95b9b5016cfc50
Nesha Mervin <nesha.mervin@gmail.com>
2024-06-25 10:55:17 -0400

    refactored code to conditionally render different code based on if modal was not equal to deleteAccount otherwise show the proper wording. created handleDeleteAccount to make a delete request to the sever sending the user data down. if account is successfully deleeted, users are navigated to logout

--- client/src/components/Delete.jsx
+++ client/src/components/Delete.jsx
@@ -12,6 +12,7 @@ export default function Delete({
   selectedTask,
   setIsBoardUpdated,
   modal,
+  user,
   closeModal,
 }) {
   const { boards } = useContext(Context);
@@ -54,41 +55,87 @@ export default function Delete({
     }
   }
 
+  async function handleDeleteAccount() {
+    try {
+      const res = await axios.delete(`${baseURL}/account/deleteAccount`, {
+        user,
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(res);
+
+      if (res.status === 200) {
+        navigate('/logout');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <Modal>
       <div className="bg-white dark:bg-darkGrey w-[343px] relative flex flex-col rounded-md p-6 md:w-[480px] md:p-8">
-        <h2 className="text-deleteRed text-lg font-semibold mb-6">
-          Delete this {`${modal === 'deleteTask' ? 'task' : 'board'}`}?
-        </h2>
-        <p className="text-mediumGrey text-[13px] leading-6 mb-6">
-          Are you sure you want to delete the
-          {`${
-            modal === 'deleteTask'
-              ? ` '${selectedTask.title}' task and its subtasks`
-              : ` '${board.name}' board`
-          }`}
-          ?{' '}
-          {`This action ${
-            modal === 'deleteBoard'
-              ? 'will remove all columns and tasks and '
-              : ' '
-          }cannot be reversed.`}
-        </p>
-
-        <div className="flex flex-col gap-4 md:flex-row">
-          <button
-            onClick={() => deleteData()}
-            className="text-white bg-deleteRed text-[13px] font-semibold leading-6 md:w-[200px] rounded-[20px] py-3 hover:bg-redHover"
-          >
-            Delete
-          </button>
-          <button
-            onClick={closeModal}
-            className="text-mainPurple bg-lightPurple dark:bg-white text-[13px] font-semibold leading-6 md:w-[200px] rounded-[20px] py-3 hover:bg-lightPurple/25"
-          >
-            Cancel
-          </button>
-        </div>
+        {modal !== 'deleteAccount' ? (
+          <>
+            <h2 className="text-deleteRed text-lg font-semibold mb-6">
+              Delete this {`${modal === 'deleteTask' ? 'task' : 'board'}`}?
+            </h2>
+            <p className="text-mediumGrey text-[13px] leading-6 mb-6">
+              Are you sure you want to delete the
+              {`${
+                modal === 'deleteTask'
+                  ? ` '${selectedTask.title}' task and its subtasks`
+                  : ` '${board.name}' board`
+              }`}
+              ?{' '}
+              {`This action ${
+                modal === 'deleteBoard'
+                  ? 'will remove all columns and tasks and '
+                  : ' '
+              }cannot be reversed.`}
+            </p>
+            <div className="flex flex-col gap-4 md:flex-row">
+              <button
+                onClick={() => deleteData()}
+                className="text-white bg-deleteRed text-[13px] font-semibold leading-6 md:w-[200px] rounded-[20px] py-3 hover:bg-redHover"
+              >
+                Delete
+              </button>
+              <button
+                onClick={closeModal}
+                className="text-mainPurple bg-lightPurple dark:bg-white text-[13px] font-semibold leading-6 md:w-[200px] rounded-[20px] py-3 hover:bg-lightPurple/25"
+              >
+                Cancel
+              </button>
+            </div>{' '}
+          </>
+        ) : (
+          <div>
+            <h2 className="text-deleteRed text-lg font-semibold mb-6">
+              Delete your Kanflow account?
+            </h2>
+            <p className="text-mediumGrey text-[13px] leading-6 mb-6">
+              Are you sure you want to delete your account along with all your
+              board(s) and tasks. This action cannot be reversed.
+            </p>
+            <div className="flex flex-col gap-4 md:flex-row">
+              <button
+                onClick={() => handleDeleteAccount()}
+                className="text-white bg-deleteRed text-[13px] font-semibold leading-6 md:w-[200px] rounded-[20px] py-3 hover:bg-redHover"
+              >
+                Delete
+              </button>
+              <button
+                onClick={closeModal}
+                className="text-mainPurple bg-lightPurple dark:bg-white text-[13px] font-semibold leading-6 md:w-[200px] rounded-[20px] py-3 hover:bg-lightPurple/25"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </Modal>
   );
